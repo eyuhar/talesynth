ec687047aae40a085f1e75149a8ea915c8de8fc9
Eyyuep <eyuep.harputlu@gmail.com>
2025-11-02 23:56:20 +0100

    add test pages

--- app/lobby/page.tsx
+++ app/lobby/page.tsx
@@ -1,11 +1,24 @@
 import { requireAuth } from "@/lib/auth";
+import { getCharacters } from "../actions/characters";
+import CharacterForm from "./CharacterForm";
 
-export default async function LobbyPage() {
+export default async function DashboardPage() {
   const user = await requireAuth();
 
+  // fetch characters server-side
+  const characters = await getCharacters();
+
   return (
-    <div>
-      <h1>Your Characters</h1>
+    <div className="min-h-screen p-6">
+      <h1 className="text-2xl font-bold mb-6">Lobby</h1>
+
+      {/* Client Component for Character creation */}
+      <CharacterForm />
+
+      {/* Client Component for Character List */}
+      {characters.characters.map((char) => (
+        <p>{char.name}</p>
+      ))}
     </div>
   );
 }
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@/lib/supabase/server";
 import { redirect } from "next/navigation";
 
+// for server actions and pages that require authentication
 export async function requireAuth() {
   const supabase = await createClient();
   const {
